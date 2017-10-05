988fa12ef525fad2b9dc6803e628603f62a4bc58
Paul Ward <pward@savicontrols.com>
2017-10-05 04:10:27 -0500

    Removed nats connection string

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Hemera = require('nats-hemera')
 const HemeraJoi = require('hemera-joi')
-const nats = require('nats').connect('nats://eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaXNoIjpbIlNBTkRCT1guKiIsIj4iXSwic3Vic2NyaWJlIjpbIlBVQkxJQy4-IiwiX0lOQk9YLj4iLCI-Il0sImlhdCI6MTUwNzAyMjM0Mn0.oZG-RL3SGjtYL6sDXyE4vhgxfgZcvdd0fxmqCZurtotOugI3y6E0jdFk5-Uv_MIMW4CzmOILs0ZZXMXwkM7f8gnnp1VYl5Bb5mhMzMfmdFiqVZ6X_vYI93rwFei1u4WH6oybDXWQK7JJrv5dAaizGJS9pIo31Qf67T4XWdtEh5LGRKloOFQVCFvmG4ZHZsSAy42SqL42Mz8_ZwTk0SIt8N4u4xTMFaatqArQlaQhtIs79nnGv6gd3LJEgW1CeP3i3qq4uECWiLvnYaWaMYrqwgMnNTyTkt2SS0o5D24cr75irqxGZXjgglJRjzcXPpgr9z2TNYQuuEtcZ7d91wRTYw@192.168.1.151:4222/')
+const nats = require('nats').connect('')
 const asCallback = require('ascallback')
 
 const hemera = new Hemera(nats, { logLevel: 'info' })
